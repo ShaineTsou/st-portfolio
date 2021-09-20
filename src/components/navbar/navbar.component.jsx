0331845bc0a8ef23d42cb572b601faf472d842c1
Shaine Tsou <shaineagro06@gmail.com>
2021-09-20 20:50:24 +0800

    made nav hidden or show on scroll, fixed accessibility issues of clickable div tags by changing them to buttons

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { useState } from "react"
 import { Link } from "gatsby"
 import { navigate } from "@reach/router"
 import { StaticImage } from "gatsby-plugin-image"
@@ -7,49 +6,102 @@ import CustomBtn from "../custom-btn/custom-btn.component"
 import { FaBars } from "react-icons/fa"
 import "./navbar.styles.scss"
 
-const Navbar = () => {
-  const [optionsHidden, toggleOptionsHidden] = useState(true)
-
-  const toggleOptions = () => {
-    toggleOptionsHidden(!optionsHidden)
+class Navbar extends React.Component {
+  state = {
+    navbarHidden: false,
+    prevScrollPos: window.pageYOffset,
+    optionsHidden: true,
   }
 
-  return (
-    <nav className={`navbar-container ${!optionsHidden ? "responsive" : ""}`}>
-      <div className="icon-container">
-        <Link to="/">
-          <StaticImage
-            src="../../images/icon.png"
-            alt="website icon"
-            placeholder="blurred"
-            layout="fixed"
-            height={50}
-          />
-        </Link>
-      </div>
-      <div className="bars" onClick={toggleOptions}>
-        <FaBars />
-      </div>
-      <div className="options-container">
-        <Link to="/" className="option">
-          Home
-        </Link>
-        <div className="option" onClick={() => navigate("#about")}>
-          About
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const currentScrollPos = window.pageYOffset
+
+    this.setState({
+      navbarHidden: currentScrollPos > this.state.prevScrollPos,
+      prevScrollPos: currentScrollPos,
+      optionsHidden: true,
+    })
+  }
+
+  toggleOptions = () => {
+    this.setState({
+      optionsHidden: !this.state.optionsHidden,
+    })
+  }
+
+  handleOptionClick = section => {
+    this.toggleOptions()
+    navigate(section)
+  }
+
+  render() {
+    const { navbarHidden, optionsHidden } = this.state
+
+    return (
+      <nav
+        className={`navbar-container ${!optionsHidden ? "responsive" : ""} ${
+          navbarHidden ? "navbar-hidden" : ""
+        }`}
+      >
+        <div className="icon-container">
+          <Link to="/">
+            <StaticImage
+              className="navbar-icon"
+              src="../../images/icon.png"
+              alt="website icon"
+              placeholder="blurred"
+              layout="fixed"
+              height={50}
+            />
+          </Link>
         </div>
-        <div className="option" onClick={() => navigate("#skills")}>
-          Skills
+        <button className="bars" onClick={this.toggleOptions}>
+          {optionsHidden ? <FaBars /> : "✕"}
+        </button>
+        <div className="options-container">
+          <button
+            className="option"
+            onClick={() => this.handleOptionClick("/")}
+          >
+            Home
+          </button>
+          <button
+            className="option"
+            onClick={() => this.handleOptionClick("#about")}
+          >
+            About
+          </button>
+          <button
+            className="option"
+            onClick={() => this.handleOptionClick("#skills")}
+          >
+            Skills
+          </button>
+          <button
+            className="option"
+            onClick={() => this.handleOptionClick("#projects")}
+          >
+            Projects
+          </button>
+          <button
+            className="option"
+            onClick={() => this.handleOptionClick("#contact")}
+          >
+            Contact
+          </button>
+          <CustomBtn>Resume</CustomBtn>
         </div>
-        <div className="option" onClick={() => navigate("#projects")}>
-          Projects
-        </div>
-        <div className="option" onClick={() => navigate("#contact")}>
-          Contact
-        </div>
-        <CustomBtn>Resume</CustomBtn>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
 
 export default Navbar
